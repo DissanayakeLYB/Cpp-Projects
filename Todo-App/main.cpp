3de1d3db099089a886a5e21917ac1e733b612f62
DissanayakeLYB <lasithdissanayake99@gmail.com>
2026-03-02 23:59:38 +0530

    create task listunder task addition

--- Todo-App/main.cpp
+++ Todo-App/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
+string tasks[100];
+int taskCount = 0;
+
 string addTask(){
     string task;
     cout << "Enter the task: ";
     getline(cin, task);
+    if (taskCount >= 100) {
+        cout << "Task list is full" << endl;
+    } else {
+        tasks[taskCount] = task;
+        taskCount++;
+    }
+    tasks[taskCount] = task;
     return task;
 }
 
